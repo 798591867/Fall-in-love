85f42a17b8d493409574d775f56ac9a76fede530
fanghongxi <798591867@qq.com>
2020-11-28 21:51:24 +0800

    添加相框页面
    Signed-off-by: fanghongxi <798591867@qq.com>

--- forwife/xinqiang/views.py
+++ forwife/xinqiang/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "xinqiang/index.html", {})
+
+def xiangce(request):
+    return render(request, "xinqiang/photokuang.html", {})
